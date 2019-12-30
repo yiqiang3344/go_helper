ce655b4d6cfaff619870aca42a5106ab993054c8
易君强 <junqiang.yi@jince.com>
2019-12-30 16:54:02 +0800

    初始化

---
+++ helper/github_app.go
@@ -0,0 +1,166 @@
+package helper
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+const clientId = "Iv1.c638d2dd2221ab39"
+const secret = "a4ae6a088f9c8868ef41c95093541ffdb4bb33f6"
+const IssuesURL = "https://api.github.com/repos/yiqiang3344/test/issues"
+
+var Token string
+var TimeLocal *time.Location
+
+type CreateIssueParams struct {
+	Title     string   `json:"title,omitempty"`
+	Body      string   `json:"body,omitempty"`
+	Assignees []string `json:"assignees,omitempty"`
+	Labels    []string `json:"labels,omitempty"`
+	State     string   `json:"state,omitempty"`
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	ClosedAt  time.Time `json:"closed_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+	Body      string // in Markdown format
+}
+
+func GetToken(w http.ResponseWriter, r *http.Request, code string, state string, redirectUri string) (string, error) {
+	//重新获取token
+	if redirectUri == "" {
+		redirectUri = "http://go.sidney.yi:8000/tool"
+	}
+
+	_url := "https://github.com/login/oauth/authorize?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&state=" + state
+	http.Redirect(w, r, _url, http.StatusFound)
+
+	//获取token
+	jsonStr := "{\"client_id\":\"" + clientId + "\",\"client_secret\":\"" + secret + "\",\"code\":\"" + code + "\"}"
+	resp, err := http.Post("https://github.com/login/oauth/access_token", "application/json", bytes.NewBuffer([]byte(jsonStr)))
+	if err != nil {
+		return "", err
+	}
+
+	// We must close resp.Body on all execution paths.
+	// (Chapter 5 presents 'defer', which makes this simpler.)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return "", fmt.Errorf("failed: %s", resp.Status)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	data, _ := url.ParseQuery(string(body))
+
+	if _, ok := data["access_token"]; !ok {
+		return "", fmt.Errorf("get token failed: %s", string(body))
+	}
+
+	WriteLog(data["access_token"][0], "sidneyyi.com/helper/github_app.go::GetToken")
+
+	return data["access_token"][0], nil
+}
+
+// SearchIssues queries the GitHub issue tracker.
+func GetIssues() ([]Issue, error) {
+	resp, err := http.Get(IssuesURL)
+	if err != nil {
+		return nil, err
+	}
+
+	// We must close resp.Body on all execution paths.
+	// (Chapter 5 presents 'defer', which makes this simpler.)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result []Issue
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+
+	WriteLog(fmt.Sprintf("%#v", result), "sidneyyi.com/helper/github_app.go::GetIssues")
+
+	return result, nil
+}
+
+func CreateIssues(data []byte) (*Issue, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", IssuesURL, bytes.NewBuffer(data))
+
+	req.Header.Add("Authorization", "token "+Token)
+	req.Header.Add("Content-Type", "application/json")
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, _ := client.Do(req)
+
+	if resp.StatusCode != http.StatusCreated {
+		resp.Body.Close()
+		return nil, fmt.Errorf("create failed: %s %s", resp.Status, string(data))
+	}
+
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//return nil, fmt.Errorf("data:%s\nresult: %s", data, string(body))
+
+	var result Issue
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
+
+func UpdateIssues(id string, data []byte) (*Issue, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("PATCH", IssuesURL+"/"+id, bytes.NewBuffer(data))
+
+	req.Header.Add("Authorization", "token "+Token)
+	req.Header.Add("Content-Type", "application/json")
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp, _ := client.Do(req)
+
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("create failed: %s %s", resp.Status, string(data))
+	}
+
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//return nil, fmt.Errorf("data:%s\nresult: %s", data, string(body))
+
+	var result Issue
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
---
+++ helper/helper.go
@@ -0,0 +1,54 @@
+package helper
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func Request(method string, url string, data []byte, header http.Header, statusCode int) (string, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
+	req.Header = header
+
+	if err != nil {
+		return "", err
+	}
+
+	resp, _ := client.Do(req)
+
+	if resp.StatusCode != statusCode {
+		resp.Body.Close()
+		return "", fmt.Errorf("status[%d]:%s", resp.StatusCode, resp.Status)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	return string(body), nil
+}
+
+func WriteLog(message string, tag string) {
+	file := "./" + time.Now().String()[0:10] + ".log"
+	logFile, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0766)
+	if nil != err {
+		panic(err)
+	}
+	loger := log.New(logFile, "", log.Ldate|log.Ltime|log.Lshortfile)
+	loger.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	loger.Println(tag + " | " + message)
+}
